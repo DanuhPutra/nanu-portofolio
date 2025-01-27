6f38cc87688eb97aa08053e0ae26c8b5cf124d64
DanuhPutra <ngurahdanuhputra@gmail.com>
2025-01-27 22:25:49 +0800

    update

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -5,12 +5,9 @@ const Navbar: React.FC = () => {
   return (
     <nav className="fixed top-0 left-0 w-full bg-gray-900 text-white py-4 px-6 z-50 shadow-lg">
       <div className="flex items-center justify-between w-[70%] mx-auto">
-        {/* Nama di sebelah kiri */}
         <div className="text-2xl font-bold">
           <Link to="/">DanuhPutra</Link>
         </div>
-
-        {/* Section navbar di sebelah kanan */}
         <ul className="flex space-x-6 text-lg">
           <li>
             <Link to="/" className="hover:text-blue-500 transition">
